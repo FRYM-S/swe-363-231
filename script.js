a608cc795bcda8419226639eae5e3284445daa94
FRYM-S <frym.edu@gmail.com>
2023-10-15 23:23:56 +0300

    JS_3-Exercise_2
    adding signature at the form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const navItems = [
+ const navItems = [
     { text: 'Home', link: '#' },
     { text: 'Services', link: '#' },
     { text: 'Projects', link: '#' },
@@ -38,4 +38,44 @@ document.addEventListener('DOMContentLoaded', function () {
                 break;
         }
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const canvas = document.getElementById('signatureCanvas');
+  const context = canvas.getContext('2d');
+  const clearButton = document.getElementById('clearSignature');
+  const signatureInput = document.getElementById('signatureInput');
+
+  let isDrawing = false;
+
+  canvas.addEventListener('mousedown', () => {
+      isDrawing = true;
+  });
+
+  canvas.addEventListener('mouseup', () => {
+      isDrawing = false;
+      saveSignature();
+  });
+
+  canvas.addEventListener('mousemove', draw);
+
+  clearButton.addEventListener('click', clearSignature);
+
+  function draw(event) {
+      if (!isDrawing) return;
+
+      context.lineWidth = 2;
+      context.lineCap = 'round';
+      context.strokeStyle = '#000';
+
+      context.lineTo(event.clientX - canvas.getBoundingClientRect().left, event.clientY - canvas.getBoundingClientRect().top);
+      context.stroke();
+      context.beginPath();
+      context.moveTo(event.clientX - canvas.getBoundingClientRect().left, event.clientY - canvas.getBoundingClientRect().top);
+  }
+
+  function clearSignature() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      signatureInput.value = ''; 
+  }
 });
